74195ab87d0c34b6a447394ffb7a960ec213c9b0
Fraan23 <fran1993teo@gmail.com>
2023-03-27 17:23:37 +0200

    Add app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const flecha_desplazar = document.querySelector("#flecha");
+
+flecha_desplazar.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
+
+// Selecciones para Barra de navegación hamburguesa
+
+const burger = document.querySelector("#menu-hamburguesa");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+// Cerrar el menú de hambuguesa cuando se hace click en un enlace  
+// Seleccionar enlaces de navegación
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
